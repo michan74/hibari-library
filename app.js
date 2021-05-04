a6294f88df0b4571f76073d47f3089578dc23c71
emi <lion3.170211.1scream@gmail.com>
2021-05-04 22:26:16 +0900

    dotenv消去

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-var config = require('dotenv').config();
+//var config = require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var menuRouter = require('./routes/menu');
